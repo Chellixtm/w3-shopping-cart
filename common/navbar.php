bc532604487a4e7d684d1c0434e504c91a5c8af2
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-30 11:14:08 -0600

    testing navbar and basic home page

---
+++ common/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-light">
+    <a class="navbar-brand" href="/index.php">
+        <img src="/images/logo-small.png" width="30" height="30" class="d-inline-block align-top" alt="Website Logo" loading="lazy">
+        Golden Apple Orchard
+    </a>
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="/view/assignments.php">Browse Apples</a>
+        </li>
+    </ul>
+    <a class="navbar-brand" href="/view/viewCart.php">
+        <img src="/images/cart-small.png" width="30" height="30" class="d-inline-block align-top" alt="Cart" loading="lazy">
+    </a>
+</nav>
---
+++ view/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Golden Apple Orchard</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Mitchell Hudson">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/style/style.css">
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/common/nav.php'; ?>
+</body>
+
+<footer>
+</footer>
+
+</html>
