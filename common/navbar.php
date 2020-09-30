dd20679cdf802e73b9cd6a7805718069f0e3d8e9
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-09-30 12:43:34 -0600

    change icons a bit

--- common/navbar.php
+++ common/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light gold-back">
     <a class="navbar-brand" href="/index.php">
-        <img src="/images/logo-small.png" width="30" height="30" class="d-inline-block align-top" alt="Website Logo" loading="lazy">
+        <img src="/images/logo-small.png" width="30" height="30" class="d-inline-block align-top icon-round" alt="Website Logo" loading="lazy">
         Golden Apple Orchard
     </a>
     <ul class="navbar-nav mr-auto">
@@ -9,6 +9,6 @@
         </li>
     </ul>
     <a class="navbar-brand" href="/view/viewCart.php">
-        <img src="/images/cart-small.png" width="30" height="30" class="d-inline-block align-top" alt="Cart" loading="lazy">
+        <img src="/images/cart-small.png" width="30" height="30" class="d-inline-block align-top icon-round" alt="Cart" loading="lazy">
     </a>
 </nav>
