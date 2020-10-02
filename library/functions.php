64fc0e198b65e4a6d00c380fbb59280ec974d692
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-10-01 21:35:48 -0600

    fixed error in builder

--- library/functions.php
+++ library/functions.php
@@ -1,7 +1,7 @@
 <?php
 function buildbrowse($apple) {
     $browsea = "<div class='col-lg'>";
-    $browsea .= "<img src='$apple[1]' class='icon-round' height='150px' width='150px />";
+    $browsea .= "<img src='/images/$apple[1]' class='icon-round' height='150px' width='150px />";
     $browsea .= "<h2>$apple[0]</h2>";
     $browsea .= "<p>Delicious apples picked fresh from our farm!</p>";
     $browsea .= "<p>Quantity Desired:</p>";
