0b75573b1070e107d4c33234bf9066d0e91a079b
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-10-02 10:53:21 -0600

    fixed unclosed form tag

--- library/functions.php
+++ library/functions.php
@@ -5,7 +5,7 @@ function buildbrowse($apple) {
     $browsea .= "<h2>$apple[0]</h2>";
     $browsea .= "<p>Delicious apples picked fresh from our farm!</p>";
     $browsea .= "<p>Quantity Desired:</p>";
-    $browsea .= "<form action='/shopping/index.php' method='post'";
+    $browsea .= "<form action='/shopping/index.php' method='post'>";
     $browsea .= "<input type='number' min='0' max='999' name='quantity'>";
     $browsea .= "<select name='size' id='size' name='type'>";
     $browsea .= "<option value='three-lbs'>3 Pounds</option>";
