778f72673c820566880b1baab6c4d56b4961c18b
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-10-01 22:10:14 -0600

    testing input size

--- library/functions.php
+++ library/functions.php
@@ -5,7 +5,7 @@ function buildbrowse($apple) {
     $browsea .= "<h2>$apple[0]</h2>";
     $browsea .= "<p>Delicious apples picked fresh from our farm!</p>";
     $browsea .= "<p>Quantity Desired:</p>";
-    $browsea .= "<input type='number' size='4'> <select name='size' id='size'>";
+    $browsea .= "<input type='number' min='0' max='999'> <select name='size' id='size'>";
     $browsea .= "<option value='three-lbs'>3 Pounds</option>";
     $browsea .= "<option value='peck'>Peck</option>";
     $browsea .= "</select>";
