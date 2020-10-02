325eed0fd7fa17c5c4da9cc9d1e0c9eba4c929c0
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-10-02 10:51:40 -0600

    testing design with form inserted

--- library/functions.php
+++ library/functions.php
@@ -5,10 +5,22 @@ function buildbrowse($apple) {
     $browsea .= "<h2>$apple[0]</h2>";
     $browsea .= "<p>Delicious apples picked fresh from our farm!</p>";
     $browsea .= "<p>Quantity Desired:</p>";
-    $browsea .= "<input type='number' min='0' max='999'> <select name='size' id='size'>";
+    $browsea .= "<form action='/shopping/index.php' method='post'";
+    $browsea .= "<input type='number' min='0' max='999' name='quantity'>";
+    $browsea .= "<select name='size' id='size' name='type'>";
     $browsea .= "<option value='three-lbs'>3 Pounds</option>";
     $browsea .= "<option value='peck'>Peck</option>";
     $browsea .= "</select>";
+    $browsea .= "</input type='submit' name='submit' value='Add to Cart'>";
+    $browsea .= "<input type='hidden' name='name' value='$apple[0]'>";
+    $browsea .= "<input type='hidden' name='action' value='add'";
+    $browsea .= "</form>";
     $browsea .= "</div>";
     return $browsea;
+}
+
+function addToCart() {
+    if(!isset($_SESSION['cart'])) {
+
+    }
 }
