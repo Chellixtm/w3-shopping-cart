27ef308185061112dcb206465912dc161491c4d7
Mitchell Hudson <mitch.hudson18@gmail.com>
2020-10-02 11:12:15 -0600

    testing an action in a header

--- shopping/index.php
+++ shopping/index.php
@@ -1,15 +1,15 @@
 <?php
 
 session_start();
-require_once $_SERVER['DOCUMENT_ROOT'].'/library/functions.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/library/functions.php';
 
 $apples = array(
-    array("Fuji","fuji.png",4,12),
-    array("Gala","gala.png",3,8),
-    array("Golden Del.","golden-delicious.png",4,12),
-    array("Granny Smith","granny-smith.png",4,12),
-    array("Honeyscrisp","honeycrisp.png",6,20),
-    array("Ambrosia","ambrosia.png",3,8)
+    array("Fuji", "fuji.png", 4, 12),
+    array("Gala", "gala.png", 3, 8),
+    array("Golden Del.", "golden-delicious.png", 4, 12),
+    array("Granny Smith", "granny-smith.png", 4, 12),
+    array("Honeyscrisp", "honeycrisp.png", 6, 20),
+    array("Ambrosia", "ambrosia.png", 3, 8)
 );
 
 $action = filter_input(INPUT_POST, 'action');
@@ -24,17 +24,20 @@ switch ($action) {
             $showbr .= buildbrowse($ap);
         }
         include $_SERVER['DOCUMENT_ROOT'] . '/view/browse.php';
-    break;
+        break;
+    case 'add':
+        header('Location: /shopping/index.php?action=browse');
+        break;
     case 'viewCart':
         include $_SERVER['DOCUMENT_ROOT'] . '/view/viewCart.php';
-    break;
+        break;
     case 'checkout':
         include $_SERVER['DOCUMENT_ROOT'] . '/view/checkout.php';
-    break;
+        break;
     case 'confirm':
         include $_SERVER['DOCUMENT_ROOT'] . '/view/confirm.php';
-    break;
+        break;
     default:
-    header('Location: /index.php');
-    break;
+        header('Location: /index.php');
+        break;
 }
